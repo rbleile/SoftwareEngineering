68ceaedfedc334a501cff51c257b339e21ed5346
k1ll3rzamb0n1 <k1ll3rzamb0n1@gmail.com>
2015-01-12 15:39:53 -0800

    ML

--- ml.js
+++ ml.js
@@ -43,7 +43,7 @@ app.set('port', process.env.PORT || 3000);
 
 var my_group = ["192.168.1.102", "192.168.1.101", "192.168.1.102"];	// replace with real IPs of group
 
-var my_index = 0;	// replace with index of my IP in my_group
+var my_index = 1;	// replace with index of my IP in my_group
 
 box.setContent('this node (' + my_group[my_index] + ') will attempt to send its token to other nodes on network. ');
 screen.render();
@@ -130,7 +130,7 @@ app.post('/do_pass', function(req, res) {
     console.log("My Responce Body");
     console.log( the_body );
 
-    box.setContent("Post with body: " + the_body);
+    box.setContent("Post with body: " + JSON.stringify( the_body ) );
 	box.style.bg = 'red';	//red for pass
 	screen.render();
 	res.json(the_body);
