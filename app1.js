8bc4f33ed76988497ca348a9d550b26e238773f2
k1ll3rzamb0n1 <k1ll3rzamb0n1@gmail.com>
2015-01-11 18:50:29 -0800

    Test

--- app1.js
+++ app1.js
@@ -39,9 +39,9 @@ screen.append(box);
 
 app.set('port', process.env.PORT || 3000);
 
-var my_group = ["192.168.1.103", "192.168.1.203"];	// replace with real IPs of group
+var my_group = ["192.168.1.100", "192.168.1.101"];	// replace with real IPs of group
 
-var my_index = 0;	// replace with index of my IP in my_group
+var my_index = 1;	// replace with index of my IP in my_group
 
 box.setContent('this node (' + my_group[my_index] + ') will attempt to send its token to other nodes on network. ');
 screen.render();
@@ -84,9 +84,11 @@ function someFunction()
 // handle PASS requests
 app.post('/do_pass', function(req, res) {
 var the_body = req.body;	//see connect package above
-//var bd = JSON.parse( the_body );
-//console.log ( "token received: " + the_body );
-        box.setContent("Post with body: " + the_body);
+
+
+console.log ( the_body );
+
+box.setContent("Post with body: " + the_body);
 	box.style.bg = 'red';	//red for pass
 	screen.render();
 //	res.json({"body": the_body, "id": JSON.stringify(my_group[my_index])});
