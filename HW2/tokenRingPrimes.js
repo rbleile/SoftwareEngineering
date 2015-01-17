3de0c3b0864591def5065961b031741f7819349c
Matt Larsen <mlarsen@cs.uoregon.edu>
2015-01-17 11:07:16 -0800

    didn't work.

--- HW2/tokenRingPrimes.js
+++ HW2/tokenRingPrimes.js
@@ -177,8 +177,6 @@ function discover()
         PostDiscover(ip);
       }
    }
-
-   if(debug) console.log("Discovery Ended. Found " + (my_group.length - 1) + " friends.");
 }
 
 /***********End Discovery***********************/
