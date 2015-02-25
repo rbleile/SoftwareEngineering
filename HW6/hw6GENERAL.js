00cf1fdd1ad7903687986d498e0f68c6326c4609
rbleile <rbleile@cs.uoregon.edu>
2015-02-24 18:01:40 -0800

    General

---
+++ HW6/hw6GENERAL.js
@@ -0,0 +1,291 @@
+var http = require('http');
+var express = require('express');
+var connect = require("connect");
+var blessed = require('blessed');
+var bodyParser = require('body-parser');
+var app = express();
+var tokenRing = require('./TokenRingManager');
+var whiteList = require('./TokenRingManager');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var debug = true;
+tokenRing.debugMessages(false);
+
+var myArgs = process.argv.slice(2);
+
+if( !myArgs[0] ) myArgs[0] = -1;
+
+var node_functionality = myArgs[0];
+
+var PICA_IP;
+
+// Create a screen object.
+
+function debugLog( msg ) 
+{
+	log.insertLine(1, ""+highestTS+" (high) : "+myTS+" (mine) : "+msg);
+	screen.render();
+	return;
+}
+
+app.set('port', process.env.PORT || 3000);
+
+//curl -H "Content-Type: application/json" -d '{"ip" : "192.168.1.101"}' http://localhost:3000/do_discover
+// handle discovery requests
+app.post('/do_discover', function(req, res) {
+	var the_body = req.body;  //see connect package above
+	if(debug) debugLog ( "discovery received: " + JSON.stringify( the_body) );
+
+	tokenRing.addRingMember(the_body.ip);
+
+	res.json({"ip": tokenRing.getMyIP(), "body" : the_body});
+});
+
+function PostDiscover(ip_address)
+{
+	var post_data = { ip : tokenRing.getMyIP() };    
+        
+	var dataString = JSON.stringify( post_data );
+
+	var headers = {
+		'Content-Type': 'application/json',
+		'Content-Length': dataString.length
+	};
+
+	var post_options = {
+		host: ip_address,
+		port: '3000',
+		path: '/do_discover',
+		method: 'POST',
+		headers: headers
+	};
+
+	var post_request = http.request(post_options, function(res){
+		res.setEncoding('utf-8');
+    
+		var responseString = '';
+
+		res.on('data', function(data){
+			responseString += data;
+		});
+
+		res.on('end', function(){
+			var resultObject = JSON.parse(responseString);
+			debugLog(resultObject);
+			tokenRing.addRingMember(resultObject.ip);
+		});
+	});
+
+	post_request.on('error', function(e) {
+		// no one is home, do nothing
+		//if(debug) debugLog('no one at this address: ' + e.message);
+	});
+
+	post_request.write(dataString);
+	post_request.end();
+}
+
+var keepAliveTimeout = 1000;
+
+function discover() 
+{
+	box.style.bg = 'red';
+    log.focus();
+    screen.render();
+
+    if(debug) debugLog("Starting Discovery");
+	//limit the scanning range
+	var start_ip = 100;
+	var end_ip   = 120;
+   
+	//we are assuming a subnet mask of 255.255.255.0
+
+	//break it up to extract what we need 
+	var ip_add = tokenRing.getMyIP().split(".");
+
+	//put it back together without the last part
+	var base_add = ip_add[0] + "." + ip_add[1] + "." + + ip_add[2] + ".";
+	if(debug) debugLog("Base ip address : " +  base_add);
+
+	for(var i = start_ip; i < end_ip; i++)
+	{      
+		var ip = base_add + i.toString();
+
+		if(!tokenRing.isMember(ip))
+		{
+			PostDiscover(ip);
+		}
+	}
+
+	setTimeout( keepAlive, keepAliveTimeout);
+}
+/***********End Discovery***********************/
+
+var count = 0;
+
+/* Function to check if other devices are there. */
+function keepAlive()
+{
+	//debugLog("Calling keepalive " );
+	var listIPs = tokenRing.getRing();
+	count++;
+	for( var i = 0; i < listIPs.length; i++) 
+	{
+		var post_data = { myIP : i };
+		if (listIPs[i] != tokenRing.getMyIP())
+		{
+			generalPOST ( listIPs[i], '/do_keepalive', post_data );
+		}
+	}
+	
+	setTimeout( keepAlive, keepAliveTimeout );
+}
+
+/*
+ * General function to replace separate functions for all different types of
+ * posts, e.g. winner, election
+ */
+function generalPOST ( genHost, genPath, post_data, err, res )
+{
+	// check if arg param err does not exist
+	if (typeof(err) != "function")
+	{
+		err = function(e) 
+		{
+			if(debug) debugLog("Lost connection to " + genHost + "removing from ring");
+
+			tokenRing.removeRingMember(genHost);
+
+			processApproval(genHost);
+
+			if(debug) debugLog("generalPOST err called "+ e);
+		};
+	}
+
+	// check if arg param res does not exist
+	if (typeof(res) != "function")
+	{
+		res = function(r) {} ;
+	}
+
+	var dataString = JSON.stringify( post_data );
+
+	var headers = {
+		'Content-Type': 'application/json',
+		'Content-Length': dataString.length
+	};
+
+	var post_options = {
+		host: genHost,
+		port: '3000',
+		path: genPath,
+		method: 'POST',
+		headers: headers
+	};
+
+	var post_request = http.request(post_options, function(res){
+		res.setEncoding('utf-8');
+		
+		var responseString = '';
+
+		res.on('data', function(data){
+			responseString += data;
+		});
+
+		res.on('end', function(){
+			//var resultObject = JSON.parse(responseString);
+		});
+	});
+	
+	post_request.on('error', err );
+	post_request.write(dataString);
+	post_request.end();
+}
+
+app.post('/do_keepalive', function(req, res) {
+	res.json(req.body);
+	var the_body = req.body;  //see connect package above
+});
+
+
+// Input 0
+var HOST_IP;
+
+//Input 1
+var TRUCK_IP;
+
+//Input 2
+var GoPiGo_IP;
+
+//Input 3
+var Grove_Sensor_IP;
+
+//Input 4
+var Human_Sensor_IP;
+
+function Broadcast_IP()
+{
+	var listIPs = tokenRing.getRing();
+	
+	var post_data = { "IP" : tokenRing.getMyIP(), "which" : node_functionality };
+	
+	for( var i = 0; i < listIPs.length; i++) 
+	{
+		if (listIPs[i] != tokenRing.getMyIP())
+		{
+			if( debug ) debugLog( "Sending to ip: " + listIPs[i] );
+			generalPOST( listIPs[i], "/gather_ips", post_data );
+		}
+	}	
+}
+
+app.post( '/gather_ips', function( req, res ){
+
+	var the_body = req.body;  //see connect package above
+	if(debug) debugLog ( "gather_ips: " + JSON.stringify( the_body) );
+
+	res.json({"ip": tokenRing.getMyIP(), "body" : the_body});
+	
+	switch the_body.which
+		case 0:
+			HOST_IP = the_body.IP;
+		case 1:
+			TRUCK_IP = the_body.IP;
+		case 2:
+			GoPiGo_IP = the_body.IP;
+		case 3:
+			Grove_Sensor_IP = the_body.IP;
+		case 4:
+			Human_Sensor_IP = the_body.IP;
+		default:
+			if(debug) debugLog( "which not Special type" + the_body.which );	
+
+
+});
+
+function releaseShotgun()
+{
+	initializePICA();
+	if(debug) debugLog("release shotgun. Current RD : " + ReqDeferred);
+	var numRequests = ReqDeferred.length;
+	for (var i = 0; i < numRequests; i++)
+	{
+		var nextPendingRequest = getNextRequestDeferred();
+		var post_data = { myIP : tokenRing.getMyIP() };
+	    if(debug) debugLog("Sending approval to : " + nextPendingRequest); 	
+		generalPOST(nextPendingRequest, '/resource_approved', post_data); 
+	}
+}
+
+// Render the screen.
+screen.render();
+
+http.createServer(app).listen(app.get('port'), function(){
+	debugLog("Express server listening on port " + app.get('port'));
+	discover();
+	debugLog( "Discovery Complete" );
+	setTimeout( Broadcast_IP, 4000  );
+});
