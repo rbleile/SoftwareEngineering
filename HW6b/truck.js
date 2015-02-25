9f263e77382cb4eb326783a26b455e55de731da2
rbleile <rbleile@cs.uoregon.edu>
2015-02-24 18:59:05 -0800

    truck merging

--- HW6b/truck.js
+++ HW6b/truck.js
@@ -374,22 +374,6 @@ app.post('/action_turninplace', function(req, res) {
     res.json(req.body);
 });
 
-/*
-function releaseShotgun()
-{
-	initializePICA();
-	if(debug) debugLog("release shotgun. Current RD : " + ReqDeferred);
-	var numRequests = ReqDeferred.length;
-	for (var i = 0; i < numRequests; i++)
-	{
-		var nextPendingRequest = getNextRequestDeferred();
-		var post_data = { myIP : tokenRing.getMyIP() };
-	    if(debug) debugLog("Sending approval to : " + nextPendingRequest); 	
-		generalPOST(nextPendingRequest, '/resource_approved', post_data); 
-	}
-}
-*/
-
 // Render the screen.
 screen.render();
 
@@ -397,5 +381,5 @@ http.createServer(app).listen(app.get('port'), function(){
 	debugLog("Express server listening on port " + app.get('port'));
 	discover();
 	debugLog( "Discovery Complete" );
-	setTimeout( Broadcast_IP, 4000  );
+	Broadcast_IP();
 });
